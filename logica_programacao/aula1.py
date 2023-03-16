5ed7ff1c9ae3ae7bd0d9a32d23627dddd2303cd2
devsirt <thuliosirt@gmail.com>
2023-03-16 12:54:47 -0300

    DocStrings e Comentários

---
+++ logica_programacao/aula1.py
@@ -0,0 +1,11 @@
+'''
+(DocString) Múltiplas linhas
+Isso não é um comentário
+Mais de uma linha
+Isso o python lê
+'''
+
+# Isso é um comentário(Linha simples)
+
+print(123)
+print(1+1)  # soma de 1 mais 1
