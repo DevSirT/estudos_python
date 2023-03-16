d3f131fdcc0f32880908a847e1b92e4966f0189a
devsirt <thuliosirt@gmail.com>
2023-03-16 13:58:10 -0300

    Tipagem, strings e aspas

---
+++ logica_programacao/aula3.py
@@ -0,0 +1,19 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> String -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print('Isso é um texto')
+print('Isso é um "texto"')  # dica trocar de aspas se quiser usar aspas
+
+# Aspas duplas
+print("Isso é um texto")
+
+
+# Escape
+print("Isso é um \"texto\"")  # \ não interpreta o proximo caracteres
+# r
+print(r"Isso é um \"texto\"")  # r expressões regulares, reparar que o \ aparece
