15183e7992a5352d97f286f7fa9df565c147e564
devsirt <thuliosirt@gmail.com>
2023-03-16 13:08:27 -0300

    função print, argumentos e quebra de linha

---
+++ logica_programacao/aula2.py
@@ -0,0 +1,5 @@
+# \r \n -> CRLF = quebrar a linha
+
+print(12, 34, end="##\n")  # exibir na tela   , adiciona um argumento
+print(56, 78, sep="")  # sep serve para separar os argumentos
+print(9, 10, 11, sep="-")
