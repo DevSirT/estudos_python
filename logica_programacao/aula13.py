768c34669eb05b1d95b06c102bac4dfc568d612a
devsirt <thuliosirt@gmail.com>
2023-03-16 21:32:51 -0300

    Solução Calculo IMC

---
+++ logica_programacao/aula13.py
@@ -0,0 +1,14 @@
+nome = 'Thulio Carlos'
+altura = 1.74
+peso = 100
+imc = peso / (altura * altura)
+
+# f-strings"
+linha = f"{nome} tem {altura:.2f} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}"
+
+# Thúlio Carlos tem 1.74 de altura,
+# pesa 100 quilos e seu IMC é
+# 33.029462280354075
+
+
+print(linha)
